0a780e2a54de22ddc81423005a9cfcc1eed677a5
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-12-13 19:00:48 +0100

    Add project files.

---
+++ FriendFace/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FriendFace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var BobArno = new User("Bob", "Arno");
+            var GusBull = new User("Gus", "Bull");
+            var JimNebb = new User("Jim", "Nebb");
+
+            BobArno.AddFriend(GusBull);
+            BobArno.ShowFriendsList();
+        }
+    }
+}
---
+++ FriendFace/User.cs
@@ -0,0 +1,32 @@
+﻿namespace FriendFace
+{
+    public class User
+    {
+        string FirstName;
+        string LastName;
+
+        List<User> MyFriends = new();
+
+        public User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void ShowFriendsList()
+        {
+            foreach (var friend in MyFriends)
+            {
+                Console.WriteLine(friend.FirstName + " " + friend.LastName);
+            }
+        }
+        public void AddFriend(User friend)
+        {
+            MyFriends.Add(friend);
+        }
+        public void RemoveFriend(User person)
+        {
+            MyFriends.Remove(person);
+        }
+    }
+}
